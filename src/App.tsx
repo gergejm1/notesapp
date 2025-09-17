14fdc1d4fa1343b7e2c395efdfa65ff84085d551
gergejm1 <130800554+gergejm1@users.noreply.github.com>
2025-09-16 20:08:34 -0400

    fixed changes for amplify

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-        <h1>Hello from Amplify</h1>
-      </header>
-    </div>
-  );
-}
-export default App;
+
+
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -40,6 +30,12 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+          <div className="App">
+      <header className="App-header">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+        <h1>Hello from Amplify</h1>
+      </header>
+    </div>
     </>
   )
 }
